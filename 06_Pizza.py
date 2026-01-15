00493fea6e1472af8c70d5c893fd1cadcf5ffeab
Ameed-Saleh <ameed.saleh@outlook.com>
2026-01-15 11:13:12 +0200

    Add files via upload

---
+++ 06_Pizza.py
@@ -0,0 +1,11 @@
+# START
+
+pizza: int  = 4
+guest: int = 5
+peices: int = pizza * 2
+sum = pizza * peices
+
+print ("each guest receives =" , sum // guest)
+print ("the number of pizza peices left is =" ,sum % guest)
+
+# STOP
