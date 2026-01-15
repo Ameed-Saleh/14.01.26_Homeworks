0856a50538c05d636d91124f3e42843eb6d7e551
Ameed-Saleh <ameed.saleh@outlook.com>
2026-01-15 10:32:34 +0200

    Add files via upload

---
+++ 05_Area_Perimeter.py
@@ -0,0 +1,10 @@
+# START
+
+width: int = 30
+height: int = 40
+area = width * height
+perimeter = 2 * (width + height)
+print("area = ", area )
+print("perimeter = ", perimeter)
+
+# STOP
---
+++ 06_Pizza.py
@@ -0,0 +1,12 @@
+# START
+
+sum: int = 0
+pizza: int  = 4
+guest: int = 5
+peices: int = pizza * 2
+sum = pizza * peices
+
+print (" each guest receives =" , sum // guest)
+print ("the number of pizza peices left is =" ,sum % guest)
+
+# STOP
